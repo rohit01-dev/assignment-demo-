bb03af146e7f0ca2ad241d3a5d31d47d923742a5
rohit01-dev <134994643+rohit01-dev@users.noreply.github.com>
2023-05-30 11:30:49 +0530

    assessment 2 complete

---
+++ day 2.js
@@ -0,0 +1,7 @@
+Sure, here's the JavaScript code to create an array called "fruits" with the mentioned fruits:
+
+```javascript
+var fruits = ["apple", "banana", "orange", "grape", "mango"];
+``` 
+
+In this code, we declare a variable called "fruits" and assign it an array of strings containing the fruits. The syntax for creating an array in JavaScript is to surround a comma-separated list of values with square brackets.
