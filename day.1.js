cd15a0779431900e1b62ea7215387fb7a1a591ee
rohit01-dev <134994643+rohit01-dev@users.noreply.github.com>
2023-05-30 11:23:56 +0530

    Day 01 assignment submitted

---
+++ day.1.js
@@ -0,0 +1,16 @@
+To create a function in JavaScript that takes two numbers as parameters and returns their sum, you can use the following code:
+
+```javascript
+function sum(num1, num2) {
+  var result = num1 + num2;
+  return result;
+}
+
+// Example usage:
+console.log(sum(10, 20)); // Output: 30
+console.log(sum(5, -2)); // Output: 3
+```
+
+In this code, the `sum` function takes two parameters, `num1` and `num2`, and calculates their sum using the `+` operator. The result is stored in the `result` variable and returned using the `return` statement. 
+
+To test the function, you can call it using two numbers as arguments and print the returned result using `console.log`.
